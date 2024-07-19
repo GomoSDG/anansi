be2be80de6d986d726c9b73ca648c6f1ea93988b
Gomotso <lilokoego@outlook.com>
2024-07-19 12:31:06 +0200

    feat: set up basic functions for querying the grid.

--- src/main/minesweeper/core.cljs
+++ src/main/minesweeper/core.cljs
@@ -14,35 +14,70 @@
       :vertical
 
       (> slope 0)
-      :right
+      :left
 
       (< slope 0)
-      :left
+      :right
 
       (zero? 0)
       :horizontal)))
 
 
 (defn initialise-grid [width height]
-  (for [x (range width)
-        y (range height)
+  {:width width
+   :height height
+   :data (->> (for [x (range width)
+                    y (range height)
 
-        :let [c (str x "," y)
-              edges (->> (for [xe (range (dec x) (+ x 2))
-                               ye (range (dec y) (+ y 2))
-                               :let [ce  (str xe "," ye)]
-                               :when (and (not= c ce) (>= xe 0) (>= ye 0) (< xe width) (< ye height))]
-                           {:cell-id ce
-                            :label (calculate-label x xe y ye)})
-                         (set))]]
-    {:x x
-     :y y
-     :neighbours edges
-     :id c}))
+                    :let [c (str x "," y)
+                          edges (->> (for [xe (range (dec x) (+ x 2))
+                                           ye (range (dec y) (+ y 2))
+                                           :let [ce  (str xe "," ye)]
+                                           :when (and (not= c ce) (>= xe 0) (>= ye 0) (< xe width) (< ye height))]
+                                       {:cell-id ce
+                                        :label (calculate-label x xe y ye)})
+                                     (set))]]
+                {:x x
+                 :y y
+                 :neighbours edges
+                 :content nil
+                 :id c})
+              (map (juxt :id identity))
+              (into {}))})
+
+
+(defn set-cell-content
+  ([grid x y content]
+   (set-cell-content grid (str x "," y) content))
+  ([grid coords content]
+   (update-in grid [:data coords] assoc :content content)))
+
+
+(defn get-cell
+  ([grid x y]
+   (get-cell grid (str x "," y)))
+  ([grid coords]
+   (get-in grid [:data coords])))
+
+
+(defn get-cell-content
+  ([grid x y]
+   (get-cell-content grid (str x "," y)))
+  ([grid coords]
+   (-> (get-cell grid coords)
+       (:content))))
 
 
 (comment
-  (initialise-grid 3 3)
+  (-> (initialise-grid 3 3)
+      (set-cell-content  0 0 :X)
+      (get-cell 0 0))
+
+  )
+
+
+(comment
+  (time (initialise-grid 2 2))
   ;; Node
   {:content :empty ; :empty | :mine
    :x 0 ; num
