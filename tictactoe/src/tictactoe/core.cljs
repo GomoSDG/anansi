0a950a09bf923d5581942174f543d5e3e4cc64cf
gomosdg <lilokoego@outlook.com>
2024-07-31 14:07:25 +0200

    feat: make algorithmn readable.

--- tictactoe/src/tictactoe/core.cljs
+++ tictactoe/src/tictactoe/core.cljs
@@ -6,20 +6,21 @@
   ([grid orientation token x y]
    (token-won? grid orientation token (str x "," y)))
   ([grid orientation token coords]
-   (loop [q #queue [(anansi/get-cell grid coords)]
-          l 0
-          t (set coords)]
-     (let [{:keys [neighbours id]} (peek q)
-           n' (->> (filter #(and (= (:orientation %) orientation)
-                                 (not (t (:cell-id %))))
-                           neighbours)
-                   (map #(anansi/get-cell grid (:cell-id %)))
-                   (filter #(= token (:data %))))]
-       (if-not (not-empty n')
-         (>= l 2)
-         (recur (into (pop q) n')
-                (inc l)
-                (conj t id)))))))
+   (loop [queue #queue [(anansi/get-cell grid coords)]
+          length 0
+          traversed (set coords)]
+     (let [{:keys [neighbours id]} (peek queue)
+           ;; Get all the neighbours that are in the required orientation and not traversed
+           neighbours (->> (filter #(and (= (:orientation %) orientation)
+                                         (not (traversed (:cell-id %))))
+                                   neighbours)
+                           (map #(anansi/get-cell grid (:cell-id %)))
+                           (filter #(= token (:data %))))]
+       (if-not (not-empty neighbours)
+         (>= length 2) ;; Win if length is meets requirement
+         (recur (into (pop queue) neighbours)
+                (inc length)
+                (conj traversed id)))))))
 
 
 (comment
